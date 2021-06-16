b61640a9df41319e185eaa84806389cd924b5630
zsanli <zhi.zhang@lns.etat.lu>
2021-06-16 14:55:28 +0200

    add find in not in list in vcf

--- draw_comparisonVenn.py
+++ draw_comparisonVenn.py
@@ -25,10 +25,20 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn2
+import re
 
 __version__ = '0.1'
 USAGE = '''
 '''
+def rm_comment(x):
+    if str(x).startswith("##"):
+        return True
+    else:
+        return False
+
+pattern1 = re.compile(r'''^##''')
+pattern11 = re.compile(r'''^#''')
+
 def main0(args):
     print(args)
     input_custom=args.input_custom
@@ -74,12 +84,65 @@ def main0(args):
     df_report_set_only2 = df_report_set_only.sort_values(['#CHROM','POS'], ascending=[True, True])
     df_two_sets_intersection2 = df_two_sets_intersection.sort_values(['#CHROM','POS'], ascending=[True, True])
 
-    df_custom_set_only2.to_csv(os.path.join(output_dir,"custom_set_only.tsv"), sep='\t',index=False, header=False)
-    df_report_set_only2.to_csv(os.path.join(output_dir,"report_set_only.tsv"), sep='\t',index=False, header=False)
-    df_two_sets_intersection2.to_csv(os.path.join(output_dir,"two_sets_intersection.tsv"), sep='\t', index=False, header=False)
+    df_custom_set_only2.to_csv(os.path.join(output_dir,"%s_custom_set_only.tsv"%(setting_name)), sep='\t',index=False, header=False)
+    df_report_set_only2.to_csv(os.path.join(output_dir,"%s_report_set_only.tsv"%(setting_name)), sep='\t',index=False, header=False)
+    df_two_sets_intersection2.to_csv(os.path.join(output_dir,"%s_two_sets_intersection.tsv"%(setting_name)), sep='\t', index=False, header=False)
+
+def main1(args):
+    print(args)
+    input_coordinate_list=args.input_coordinate_list
+    input_vcf=args.input_vcf
+    setting_name=args.setting
+    
+    output_dir = os.path.dirname(input_coordinate_list)
+    input_coordinate_list_data=pd.read_csv(input_coordinate_list,header=None,delimiter="\t")
+    input_coordinate_list_data.columns = ["#CHROM","POS"]
+#    input_vcf_data=pd.read_csv(input_vcf,delimiter="\t", header = 1, skiprows=lambda x: rm_comment(x))
+    input_vcf_data =[]
+    with open(input_vcf, "r") as fh:
+        while True:
+            line = fh.readline()
+            if not line:
+                break
+            elif re.search(pattern1, line):
+                pass
+            else:
+                input_vcf_data.append(line.rsplit("\t"))
+    
+    input_vcf_data2 =  pd.DataFrame(input_vcf_data[1:]) 
+    input_vcf_data2.columns = input_vcf_data[0]
+    input_vcf_data2["POS"] = input_vcf_data2["POS"].astype(int)   
+    chr_index = pd.CategoricalDtype(["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y"], ordered = True)
+
+#    df3 = input_coordinate_list_data.set_index(["#CHROM","POS"])
+#    df3.update(input_vcf_data2.set_index(["#CHROM","POS"]))
+#    df3.reset_index()
+
+    a = set(list(zip(list(input_coordinate_list_data["#CHROM"]),list(input_coordinate_list_data["POS"]))))
+    b = set(list(zip(list(input_vcf_data2["#CHROM"]),list(input_vcf_data2["POS"]))))
+    coordinate_list_not_in_vcf = list(a.difference(b))
+    coordinate_list_in_vcf = list(a & b)
+    pd_coordinate_list_not_in_vcf = pd.DataFrame(coordinate_list_not_in_vcf)
+    pd_coordinate_list_in_vcf = pd.DataFrame(coordinate_list_in_vcf)
+
+    pd_coordinate_list_not_in_vcf.columns = ["#CHROM","POS"]
+    pd_coordinate_list_in_vcf.columns = ["#CHROM","POS"]
+
+    pd_coordinate_list_not_in_vcf['#CHROM'] = pd_coordinate_list_not_in_vcf['#CHROM'].astype(chr_index)
+    pd_coordinate_list_in_vcf['#CHROM'] = pd_coordinate_list_in_vcf['#CHROM'].astype(chr_index)
+
+    pd_coordinate_list_not_in_vcf['POS'] = pd_coordinate_list_not_in_vcf['POS'].astype('int')
+    pd_coordinate_list_in_vcf['POS'] =  pd_coordinate_list_in_vcf['POS'].astype('int')
 
 
 
+    pd_coordinate_list_not_in_vcf2 = pd_coordinate_list_not_in_vcf.sort_values(['#CHROM','POS'], ascending=[True, True])
+    pd_coordinate_list_in_vcf2 = pd_coordinate_list_in_vcf.sort_values(['#CHROM','POS'], ascending=[True, True])
+    
+
+    pd_coordinate_list_not_in_vcf2.to_csv(os.path.join(output_dir,"_coordinate_list_NOT_in_vcf.tsv"%(setting_name)), sep='\t',index=False, header=False)
+    pd_coordinate_list_in_vcf2.to_csv(os.path.join(output_dir,"_coordinate_list_in_vcf.tsv"%(setting_name)), sep='\t',index=False, header=False)
+    
 #    with open(os.path.join(output_dir,"custom_set_only.tsv"),"w+") as fh:
 #        for i in custom_set_only:
 #            fh.write("".join([i.split(":")[0].replace("chr",""),"\t",i.split(":")[1],"\n"]))
@@ -91,7 +154,6 @@ def main0(args):
 #    with open(os.path.join(output_dir,"two_sets_intersection.tsv"),"w+") as fh:
 #        for i in two_sets_intersection:
 #            fh.write("".join([i.split(":")[0].replace("chr",""),"\t",i.split(":")[1],"\n"]))
-
 
 
 def main():
@@ -107,6 +169,16 @@ def main():
     parser_0.add_argument('-s', '--setting',
                           type=str, help='the setting name')
     parser_0.set_defaults(func=main0)
+
+    parser_1 = subparsers.add_parser('1', description='split coordinate list from vcf',
+                                     help='split coordinate list from vcf', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_1.add_argument('-i', '--input_coordinate_list',
+                          type=str, help='input coordinate list')
+    parser_1.add_argument('-r', '--input_vcf',
+                          type=str, help='the input vcf')
+    parser_1.add_argument('-s', '--setting',
+                          type=str, help='the setting name')
+    parser_1.set_defaults(func=main1)
 
     args = parser.parse_args(sys.argv[1:])
     args.func(args)
